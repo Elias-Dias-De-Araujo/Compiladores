89047ea2088a2520535fa55460863f17488d5321
Elias_Dias <eliasdiasdearaujo2@gmail.com>
2022-06-15 16:10:41 -0300

    Criando árvore

--- Otimizacao_de_codigo/Binary_tree.java
+++ Otimizacao_de_codigo/Binary_tree.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 
 public class Binary_tree {
     private Node root;
+    private Node nodeFinded = null;
     private int plus, less, mult, div, move, mem = 0;
     private String row;
     private int current = 1;
@@ -33,47 +34,104 @@ public class Binary_tree {
 
     public void creatingTree() {
         while (!cutRow.get(current).equals(" ")) {
+            int sumQuantSides = 0;
+            String[] splited = null;
+            String aux = "";
             String reserved = cutRow.get(current);
             ArrayList<String> newArrayHash = new ArrayList<String>();
             if (cutRow.get(current + 1).equals("(")) {
                 reserved = changeReservedName(reserved);
                 Node newNode = new Node(reserved);
-                if (!scopes.isEmpty()) {
-                    tablePatterns.get(scopes.get(scopes.size() - 1)).add(cutRow.get(current));
+                if (scopes.isEmpty()) {
+                    this.root = newNode;
+                } else {
+                    splited = scopes.get(scopes.size() - 1).split(" ");
+                    aux = splited[0] + " " + splited[1];
+                    inOrderFind(root, aux);
+                    if (splited[2].equalsIgnoreCase("0")) {
+                        nodeFinded.setLeft(newNode);
+                    } else if (splited[2].equalsIgnoreCase("1")) {
+                        nodeFinded.setRight(newNode);
+                    }
+                    sumQuantSides = Integer.parseInt(splited[2]) + 1;
+                    aux = aux + " " + sumQuantSides;
+                    nodeFinded = null;
+                    // tablePatterns.get(scopes.get(scopes.size() - 1)).add(cutRow.get(current));
                 }
-                tablePatterns.put(cutRow.get(current), newArrayHash);
-                scopes.add(cutRow.get(current));
+                if (!scopes.isEmpty()) {
+                    scopes.set(scopes.size() - 1, aux); // Aqui é para atualizar o valor de side disponivel no nó atual
+                }
+                scopes.add(reserved + " 0"); // adicionando novo nó
                 current += 1;
             } else if (cutRow.get(current).equals(")")) {
                 scopes.remove(scopes.size() - 1);
             } else if (!cutRow.get(current).equals(",") && !cutRow.get(current).equals("(")
                     && !cutRow.get(current).equals(")")) {
                 if (cutRow.get(current + 1).equals(",") || cutRow.get(current + 1).equals(")")) {
-                    tablePatterns.get(scopes.get(scopes.size() - 1)).add(cutRow.get(current));
+                    reserved = changeReservedName(reserved);
+                    Node newNode = new Node(reserved);
+
+                    splited = scopes.get(scopes.size() - 1).split(" ");
+                    aux = splited[0] + " " + splited[1];
+                    inOrderFind(root, aux);
+                    if (splited[2].equalsIgnoreCase("0")) {
+                        nodeFinded.setLeft(newNode);
+                    } else if (splited[2].equalsIgnoreCase("1")) {
+                        nodeFinded.setRight(newNode);
+                    }
+                    sumQuantSides = Integer.parseInt(splited[2]) + 1;
+                    aux = aux + " " + sumQuantSides;
+                    nodeFinded = null;
+
+                    // tablePatterns.get(scopes.get(scopes.size() - 1)).add(cutRow.get(current));
                 } else {
                     String getTogether = cutRow.get(current) + " " + cutRow.get(current + 1);
-                    tablePatterns.get(scopes.get(scopes.size() - 1)).add(getTogether);
+
+                    Node newNode = new Node(getTogether);
+
+                    splited = scopes.get(scopes.size() - 1).split(" ");
+                    aux = splited[0] + " " + splited[1];
+                    inOrderFind(root, aux);
+                    if (splited[2].equalsIgnoreCase("0")) {
+                        nodeFinded.setLeft(newNode);
+                    } else if (splited[2].equalsIgnoreCase("1")) {
+                        nodeFinded.setRight(newNode);
+                    }
+                    sumQuantSides = Integer.parseInt(splited[2]) + 1;
+                    aux = aux + " " + sumQuantSides;
+                    if (!scopes.isEmpty()) {
+                        scopes.set(scopes.size() - 1, aux); // Aqui é para atualizar o valor de side disponivel no nó
+                                                            // atual
+                    }
+                    nodeFinded = null;
+
+                    // tablePatterns.get(scopes.get(scopes.size() - 1)).add(getTogether);
                     current += 1;
                 }
             }
             current += 1;
         }
 
-        printing();
+        printing(root);
     }
 
-    public void printing() {
-        String Tree = "";
-        for (String key : tablePatterns.keySet()) {
-            if (tablePatterns.get(key).size() == 1) {
-                Tree += key + "--|" + tablePatterns.get(key).get(0);
-            } else if (tablePatterns.get(key).size() > 1) {
-                Tree += "---|";
-                Tree += tablePatterns.get(key).get(0); // Esquerda
-                Tree += tablePatterns.get(key).get(1); // Direita
+    public void printing(Node current) {
+        if (current != null) {
+            printing(current.getLeft());
+            System.out.println(current.getValue());
+            printing(current.getRight());
+        }
+    }
+
+    public void inOrderFind(Node current, String valueToFind) {
+        if (current.getValue().equalsIgnoreCase(valueToFind)) {
+            nodeFinded = current;
+        } else {
+            inOrderFind(current.getLeft(), valueToFind);
+            if (nodeFinded == null) {
+                inOrderFind(current.getRight(), valueToFind);
             }
         }
-        System.out.println(Tree);
     }
 
     public String changeReservedName(String reserved) {
@@ -101,3 +159,8 @@ public class Binary_tree {
     }
 
 }
+
+/*
+ * Node Estrutura:
+ * Nome do Node | identificador | Quantidade de nós
+ */
